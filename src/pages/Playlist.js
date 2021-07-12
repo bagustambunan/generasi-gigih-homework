d366ba4eba0ff289cd213112332950c004a11c6e
Muhammad Bagus Syahputra Tambunan <mhdbagus78@gmail.com>
2021-07-12 22:27:48 +0700

    Add data to view

--- src/pages/Playlist.js
+++ src/pages/Playlist.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import data from '../data/data_playlist'
 
-class Search extends React.Component {
+class Playlist extends React.Component {
 
     render() {
         return (
-            "Test"
+            <div class="box" id="box_playlists">
+                <div id="box_img">
+                    <img src={data.album.images[0].url} alt="Album image"/>
+                </div>
+                <div>
+                    <p id="text_title">{data.album.name}</p>
+                </div>
+                <div>
+                    <p id="text_artist">{data.album.artists[0].name}</p>
+                </div>
+                <div>
+                    <p id="text_album">{data.name}</p>
+                </div>
+                <div>
+                    <button>Select</button>
+                </div>
+            </div>
         );
     }
 }
+
+export default Playlist;
