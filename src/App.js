f5c36fcf840dc730ce887b6598161dd3f561987d
Muhammad Bagus Syahputra Tambunan <mhdbagus78@gmail.com>
2021-07-12 22:17:13 +0700

    Add playlist view

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Playlist from './pages/Playlist';
 
 function App() {
 
@@ -7,22 +8,23 @@ function App() {
   console.log(spotify_client_id);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+    <Playlist/>
   );
 }
 
---
+++ src/pages/Playlist.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class Search extends React.Component {
+
+    render() {
+        return (
+            "Test"
+        );
+    }
+}
