09f718d238200c307787de0b6f4c3a5e7137a372
Muhammad Bagus Syahputra Tambunan <mhdbagus78@gmail.com>
2021-07-14 20:15:56 +0700

    Unlink Material UI Core and Icon

--- src/pages/Playlist.js
+++ src/pages/Playlist.js
@@ -5,40 +5,40 @@ import { makeStyles } from '@material-ui/core/styles';
 import { Card, CardActionArea, CardActions, CardContent, CardMedia, Button, Typography } from '@material-ui/core';
 
 const useStyles = makeStyles({
-    root: {
-      maxWidth: 345,
-    },
-  });
+  root: {
+    maxWidth: 345,
+  },
+});
 
-  export default function Playlist() {
-    const classes = useStyles();
-  
-    return (
-      <Card className={classes.root}>
-        <CardActionArea>
-          <CardMedia
-            component="img"
-            alt="Contemplative Reptile"
-            height="250"
-            image={data.album.images[0].url}
-            title="Contemplative Reptile"
-          />
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="h2">
-                {data.album.name}
-            </Typography>
-            <Typography variant="body2" color="textSecondary" component="p">
-                Artist: {data.album.artists[0].name}
-                <br/>
-                Album: {data.name}
-            </Typography>
-          </CardContent>
-        </CardActionArea>
-        <CardActions>
-          <Button size="small" color="primary">
-            Select
-          </Button>
-        </CardActions>
-      </Card>
-    );
-  }
+export default function Playlist() {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          alt="Contemplative Reptile"
+          height="250"
+          image={data.album.images[0].url}
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+              {data.album.name}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+              Artist: {data.album.artists[0].name}
+              <br/>
+              Album: {data.name}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Select
+        </Button>
+      </CardActions>
+    </Card>
+  );
+}
