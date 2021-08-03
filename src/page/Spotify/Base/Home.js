43e476908a58b42e60d1307934f63079a1c0b316
Muhammad Bagus Syahputra Tambunan <mhdbagus78@gmail.com>
2021-08-03 18:57:41 +0700

    Edit style in base page

--- src/page/Spotify/Base/Home.js
+++ src/page/Spotify/Base/Home.js
@@ -4,6 +4,8 @@ import { useSelector, useDispatch } from "react-redux";
 import { selectToken } from "../../../redux/tokenSlice";
 import { setUser, selectUser } from "../../../redux/userSlice";
 
+import "../../../styles/base-page.css";
+
 const axios = require("axios");
 
 function Home() {
@@ -36,7 +38,6 @@ function Home() {
     return (
       <a
         href="/logout"
-        className="my-1 border-2 border-red-600 hover:bg-red-600 rounded-full text-red-600 hover:text-white text-sm font-medium px-3 py-1 cursor-pointer"
       >
         LOGOUT
       </a>
@@ -45,22 +46,21 @@ function Home() {
 
   function UserCard() {
     return (
-      <div className="p-5 bg-me_card shadow rounded-xl w-4/12">
-        <div className="flex flex-wrap">
-          <div className="mr-5">
+      <div className="home-page">
+        <div className="user-card">
+          <div className="image">
             <img
               src={user.images[0].url}
               title={user.display_name}
               alt={user.display_name}
-              className="object-cover rounded-full w-16 h-16"
             />
           </div>
-          <div className="mr-5">
-            <span className="text-lg font-bold text-me_dark_half">
+          <div className="info">
+            <span>
               {user.display_name}
             </span>
           </div>
-          <div className="text-right">
+          <div className="btn-logout">
             <LogoutButton />
           </div>
         </div>
--- src/page/Spotify/Base/LoginPage.js
+++ src/page/Spotify/Base/LoginPage.js
@@ -2,6 +2,8 @@ import React from "react";
 import { client_id, scope, redirect_uri } from "../../../values";
 import { public_url } from "../../../values";
 
+import "../../../styles/base-page.css";
+
 function LoginPage() {
   function LoginButton() {
     let spotify_url = "https://accounts.spotify.com/authorize";
@@ -12,7 +14,6 @@ function LoginPage() {
 
     return (
       <a
-        className="bg-me_primary hover:bg-gray-600 w-52 rounded-full text-white font-medium px-1 py-2 flex cursor-pointer justify-center align-middle"
         href={spotify_url}
       >
         LOG IN WITH SPOTIFY
@@ -21,28 +22,18 @@ function LoginPage() {
   }
 
   return (
-    <div className="bg-me_main min-h-screen">
-      <center>
-        <span
-          className="hidden sm:inline-block sm:align-middle sm:h-screen"
-          aria-hidden="true"
-        >
-          &#8203;
-        </span>
-
-        <center className="inline-block align-bottom justify-center transform sm:my-8 sm:align-middle ">
-          <div className="mb-10 inline-block">
-            <img
-              src={public_url + "/img/logo.png"}
-              alt="Icon"
-              className="w-36"
-            />
-          </div>
-          <div className="">
-            <LoginButton />
-          </div>
-        </center>
-      </center>
+    <div className="login-page">
+      <div className="login-card">
+        <div className="image">
+          <img
+            src={public_url + "/img/logo.png"}
+            alt="Icon"
+          />
+        </div>
+        <div className="btn-login">
+          <LoginButton />
+        </div>
+      </div>
     </div>
   );
 }
