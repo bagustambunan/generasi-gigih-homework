078acca524fb7282b602916defce5b78641d7c5d
Muhammad Bagus Syahputra Tambunan <mhdbagus78@gmail.com>
2021-08-10 23:49:34 +0700

    Test in track component (album image, duration text)

--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,34 @@
 import { render, screen } from '@testing-library/react';
+import { Provider } from "react-redux";
+import store from "./redux/store";
+
 import Track from './components/TrackItem';
 
 test('render track component', () => {
-  render(<Track/>);
+  render(
+    <Provider store={store}>
+      <Track/>
+    </Provider>
+  );
   screen.debug();
 });
 
-// test('inspect track component', () => {
-//   render(<Track/>);
-//   const gif_title = screen.getByTestId('gif_title');
-//   expect(gif_title).toBeInTheDocument();
-// });
+test('test track component: album image', () => {
+  render(
+    <Provider store={store}>
+      <Track/>
+    </Provider>
+  );
+  const album_img = screen.getByAltText("Album image")
+  expect(album_img).toBeInTheDocument();
+});
+
+test('test track component: duration text', () => {
+  render(
+    <Provider store={store}>
+      <Track/>
+    </Provider>
+  );
+  const duration_text = screen.getByText(/:/i)
+  expect(duration_text).toBeInTheDocument();
+});
