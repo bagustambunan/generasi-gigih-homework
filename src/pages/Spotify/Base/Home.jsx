e9478c0e0db7f5ab438f66d58ed6d78186387eee
Muhammad Bagus Syahputra Tambunan <mhdbagus78@gmail.com>
2021-08-19 00:42:59 +0700

    ESLint src/pages/Spotify/Base/Home.jsx

--- src/pages/Spotify/Base/Home.jsx
+++ src/pages/Spotify/Base/Home.jsx
@@ -1,27 +1,23 @@
-import { useState, useEffect } from "react";
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { selectToken } from '../../../redux/tokenSlice';
+import { setUser, selectUser } from '../../../redux/userSlice';
+import '../../../styles/base-page.css';
 
-import { useSelector, useDispatch } from "react-redux";
-import { selectToken } from "../../../redux/tokenSlice";
-import { setUser, selectUser } from "../../../redux/userSlice";
-
-import "../../../styles/base-page.css";
-
-const axios = require("axios");
+const axios = require('axios');
 
 function Home() {
-
   const dispatch = useDispatch();
   const token = useSelector(selectToken);
   const user = useSelector(selectUser);
-  const [theme, set_theme] = useState(localStorage.getItem('theme'));
 
   async function getUserInfo() {
     try {
-      let url = "https://api.spotify.com/v1/me";
+      const url = 'https://api.spotify.com/v1/me';
       await axios
         .get(url, {
           headers: {
-            Authorization: "Bearer " + token,
+            Authorization: 'Bearer ' + token,
           },
         })
         .then((res) => {
@@ -63,39 +59,21 @@ function Home() {
   }
 
   function changeTheme(themeName) {
-    set_theme(themeName);
     localStorage.setItem('theme', themeName);
     document.documentElement.className = themeName;
   }
 
   function ToggleMode(props){
-    return(
+    return (
       <div className="mode-wrapper">
         <div
-          class="btn-mode"
-          style={{backgroundColor: props.theme_color}}
+          className="btn-mode"
+          style={ { backgroundColor: props.theme_color } }
           onClick={() => {
             changeTheme(props.theme_code);
           }}
         >
-          <span>
-          </span>
         </div>
-      </div>
-    )
-  }
-
-  function ResetMode(){
-    return(
-      <div
-        class="btn-mode"
-        onClick={() => {
-          localStorage.removeItem('theme');
-        }}
-      >
-        <span>
-          RESET
-        </span>
       </div>
     )
   }
@@ -104,19 +82,23 @@ function Home() {
     getUserInfo();
   }, []);
 
-  return(
+  return (
     <>
       {user && <UserCard />}
       <div className="home-action">
-          <ToggleMode theme_code="theme-light-green" theme_color="#1ED760"/>
-          <ToggleMode theme_code="theme-dark-yellow" theme_color="#FFED4C"/>
-          <ToggleMode theme_code="theme-light-pink" theme_color="#FA6791"/>
-          <ToggleMode theme_code="theme-dark-blue" theme_color="#4AA8E6"/>
+        <ToggleMode theme_code="theme-light-green" theme_color="#1ED760" />
+        <ToggleMode theme_code="theme-dark-yellow" theme_color="#FFED4C" />
+        <ToggleMode theme_code="theme-light-pink" theme_color="#FA6791" />
+        <ToggleMode theme_code="theme-dark-blue" theme_color="#4AA8E6" />
       </div>
       <div className="home-me">
-        <span>Made with ❤️ by <a href="https://www.linkedin.com/in/bagustambunan/">Bagus Tambunan</a></span>
+        <span>
+          Made with ❤️ by
+          <a href="https://www.linkedin.com/in/bagustambunan/">
+            Bagus Tambunan
+          </a>
+        </span>
       </div>
-      
     </>
   );
 }
