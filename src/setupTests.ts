9889be8ee9158cf744a70237e27cc37c39bf22dc
Muhammad Bagus Syahputra Tambunan <mhdbagus78@gmail.com>
2021-08-18 15:13:04 +0700

    ESLint src/reportWebVitals.ts

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,5 +1,5 @@
-import "@testing-library/jest-dom/extend-expect";
-import { server } from "./mocks/server";
+import '@testing-library/jest-dom/extend-expect';
+import { server } from './mocks/server';
 
 beforeAll(() => {
   server.listen();
