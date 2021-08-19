bcaf07af1ef24658e7a32292c18ec7f8da1a0cd4
Muhammad Bagus Syahputra Tambunan <mhdbagus78@gmail.com>
2021-08-19 20:51:02 +0700

    eslint src/values/index.ts

--- src/values/index.ts
+++ src/values/index.ts
@@ -2,4 +2,4 @@ export const publicUrl = process.env.PUBLIC_URL;
 export const rootUrl = process.env.REACT_APP_ROOT_URL;
 export const redirectUri = process.env.REACT_APP_REDIRECT_URL;
 export const clientID = process.env.REACT_APP_SPOTIFY_CLIENT_ID;
-export const scope = "playlist-modify-private playlist-modify-public user-read-private playlist-read-private playlist-read-collaborative";
+export const scope = 'playlist-modify-private playlist-modify-public user-read-private playlist-read-private playlist-read-collaborative';
