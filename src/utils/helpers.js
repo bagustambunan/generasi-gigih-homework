bc39b8816861ee090904211f96e116cdc7609088
Muhammad Bagus Syahputra Tambunan <mhdbagus78@gmail.com>
2021-08-19 20:53:54 +0700

    eslint src/utils/helpers.js

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,15 +1,15 @@
 export function getDuration(duration) {
-  let minutes = Number((duration / 60000).toFixed(0));
+  const minutes = Number((duration / 60000).toFixed(0));
   let seconds = Number(((duration % 60000) / 1000).toFixed(0));
   if (seconds < 10) seconds = `0${seconds}`;
   return `${minutes}:${seconds}`;
 }
 
 export function getHashParams() {
-  let hashParams = {};
-  let e,
-    r = /([^&;=]+)=?([^&;]*)/g,
-    q = window.location.hash.substring(1);
+  const hashParams = {};
+  const r = /([^&;=]+)=?([^&;]*)/g;
+  const q = window.location.hash.substring(1);
+  let e;
   while ((e = r.exec(q))) {
     hashParams[e[1]] = decodeURIComponent(e[2]);
   }
