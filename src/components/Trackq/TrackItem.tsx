9fc8a16fbb009c6a3d3fd89c9946d5c04555b355
Muhammad Bagus Syahputra Tambunan <mhdbagus78@gmail.com>
2021-08-27 01:15:49 +0700

    Fix case

---
+++ src/components/Trackq/TrackItem.tsx
@@ -0,0 +1,75 @@
+import { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { updateSelectedTrack } from '../../redux/selectedTrackSlice';
+import { getDuration } from '../../libs/helpers';
+import { trackItemType } from '../../libs/types';
+
+function Track({
+  imageUrl,
+  trackTitle,
+  artistName,
+  albumName,
+  duration,
+  data,
+  setView,
+  selectMode,
+  highlightTracks,
+  setHighlightTracks,
+}: trackItemType) {
+  const dispatch = useDispatch();
+  const [isHighlight, setIsHighlight] = useState(highlightTracks.some((item) => item === data.uri));
+
+  useEffect(() => {
+    setIsHighlight(highlightTracks.some((item) => item === data.uri));
+  }, [highlightTracks]);
+
+  return (
+    <div
+      onClick={() => {
+        if (selectMode) {
+          if (isHighlight) {
+            const index = highlightTracks.indexOf(data.uri);
+            if (index !== -1) highlightTracks.splice(index, 1);
+            setIsHighlight(false);
+          } else {
+            setHighlightTracks([...highlightTracks, data.uri]);
+            setIsHighlight(true);
+          }
+        } else {
+          setView('trackdetail');
+          dispatch(updateSelectedTrack(data));
+        }
+      }}
+      className={'track' + (isHighlight ? ' track-highlight' : '')}
+    >
+      <div className="image">
+        <img
+          src={imageUrl}
+          title={albumName}
+          alt={albumName}
+        />
+      </div>
+
+      <div className="title">
+        <div className="track-title">
+          <span>{trackTitle}</span>
+        </div>
+        <div className="artist-name">
+          <span>
+            {artistName}
+          </span>
+        </div>
+      </div>
+
+      <div className="album">
+        <span>{albumName}</span>
+      </div>
+
+      <div className="duration">
+        <span>{getDuration(duration)}</span>
+      </div>
+    </div>
+  );
+}
+
+export default Track;
