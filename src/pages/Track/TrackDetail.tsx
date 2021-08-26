65a77e324d981af4c8806006bbc3a5ca69dd4a95
Muhammad Bagus Syahputra Tambunan <mhdbagus78@gmail.com>
2021-08-26 23:25:39 +0700

    Fix pages/Track

--- src/pages/Track/TrackDetail.tsx
+++ src/pages/Track/TrackDetail.tsx
@@ -1,8 +1,8 @@
 import { useState } from 'react';
 import { useSelector } from 'react-redux';
-import { selectSelectedTrack } from '../../../redux/selectedTrackSlice';
-import AddToPlaylist from '../../../components/modals/AddToPlaylist';
-import '../../../styles/track-page.css';
+import { selectSelectedTrack } from '../../redux/selectedTrackSlice';
+import AddToPlaylist from '../../components/modals/AddToPlaylist';
+import '../../styles/track-page.css';
 
 function TrackDetail() {
   const selecedTrack = useSelector(selectSelectedTrack);
