f30820e63cb2d64992d74c5ed5b59f812571f06f
Muhammad Bagus Syahputra Tambunan <mhdbagus78@gmail.com>
2021-08-26 23:50:51 +0700

    Refactor and fix TrackItem.test.js

--- src/components/track/TrackItem.test.js
+++ src/components/track/TrackItem.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
-import store from '../redux/store';
-import Track from '../components/Track/TrackItem';
+import store from '../../redux/store';
+import Track from './TrackItem';
 
 const imageUrl = 'https://play-lh.googleusercontent.com/v4sg6Itovu1ccaCaiHW2HwUy0vuzuy02sWXXkjxhPd-hQzVe7_FpIow48JwNMViC8Q';
 const trackTitle = 'Cute Duduu';
