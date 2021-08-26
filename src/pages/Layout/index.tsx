5181a377c35c7b983e446fae9f681a719afde92c
Muhammad Bagus Syahputra Tambunan <mhdbagus78@gmail.com>
2021-08-26 23:46:13 +0700

    Fix pages/Layout

--- src/pages/Layout/index.tsx
+++ src/pages/Layout/index.tsx
@@ -6,7 +6,7 @@ import Home from '../Home/Home';
 import PlaylistPage from '../Playlist';
 import SearchPage from '../Search';
 import NewForm from '../Playlist/NewForm';
-import { spotifyPageType } from '../../types/types';
+import { spotifyPageType } from '../../libs/types';
 import { rootUrl } from '../../libs/values';
 
 const axios = require('axios');
